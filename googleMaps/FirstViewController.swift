aa45122ad62929f7af8be6977812f0a901094253
mantovanirian <mantovanirian@gmail.com>
2019-03-23 22:08:36 +0700

    add google maps

--- googleMaps/FirstViewController.swift
+++ googleMaps/FirstViewController.swift
@@ -7,14 +7,43 @@
 //
 
 import UIKit
+import CoreLocation
+import GoogleMaps
 
 class FirstViewController: UIViewController {
-
+    
+    private let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        locationManager.requestAlwaysAuthorization()
+        locationManager.requestWhenInUseAuthorization()
+        
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+        }
     }
 
 
+}
+
+
+extension FirstViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let coordinate = manager.location?.coordinate else { return }
+         let camera = GMSCameraPosition.camera(withLatitude: coordinate.latitude, longitude: coordinate.longitude, zoom: 6.0)
+        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        view = mapView
+        
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: coordinate.latitude, longitude: coordinate.longitude)
+        marker.title = "Sydney"
+        marker.snippet = "Australia"
+        marker.map = mapView
+        view = mapView
+    }
 }
 
