b8a693e4e7c981436ae9b986e28340e226d9f2c0
mantovanirian <mantovanirian@gmail.com>
2019-03-23 21:27:47 +0700

    Initial Commit

---
+++ googleMapsTests/googleMapsTests.swift
@@ -0,0 +1,34 @@
+//
+//  googleMapsTests.swift
+//  googleMapsTests
+//
+//  Created by Rian Putra Mantovani on 23/03/19.
+//  Copyright © 2019 Rian Putra Mantovani. All rights reserved.
+//
+
+import XCTest
+@testable import googleMaps
+
+class googleMapsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
